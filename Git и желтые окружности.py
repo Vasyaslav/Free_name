6ef1962300bebad6e7ff770b427fc6ee289fae53
Vasyaslav <rkbrktyl@gmail.com>
2020-11-26 20:50:16 +0300

    Я не знаю, что происходит, но делаю.

---
+++ Git и желтые окружности.py
@@ -0,0 +1,35 @@
+import sys
+from random import choice
+from PyQt5.QtGui import QPainter, QColor, QPen
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from untitled import Ui_MainWindow
+
+
+class Sec(Ui_MainWindow, QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.pushButton.clicked.connect(self.let_paint)
+        self.can_paint = False
+
+    def let_paint(self):
+        self.can_paint = True
+        self.repaint()
+
+    def paintEvent(self, event):
+        if self.can_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_circle(qp)
+            qp.end()
+
+    def draw_circle(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        diameter = choice(range(20, 60)) * 2
+        qp.drawEllipse(60, 60, diameter, diameter)
+
+
+app = QApplication(sys.argv)
+ex = Sec()
+ex.show()
+sys.exit(app.exec())
